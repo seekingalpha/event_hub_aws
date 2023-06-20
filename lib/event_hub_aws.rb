adbeb769c97c9dda8db3333b0f23c71b28bb00a0
Bogdan Guban <bogdan@seekingalpha.com>
2023-06-20 13:26:58 +0300

    Initial commit

---
+++ lib/event_hub_aws.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'event_hub_aws/version'
+
+module EventHubAws
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/event_hub_aws_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe EventHubAws do
+  it 'has a version number' do
+    expect(EventHubAws::VERSION).not_to be_nil
+  end
+end
